0523550064a7c503adf76ae3d648a96b6bad49e7
Peshi2312 <155888766+Peshi2312@users.noreply.github.com>
2024-04-30 10:00:53 +0300

    Add files via upload

---
+++ Consolegame/Program.cs
@@ -0,0 +1,24 @@
+﻿Random randomnumber = new Random();
+int computernumber = randomnumber.Next(1, 101);
+while (true)
+{
+    Console.Write($"Guess a number (1-100): ");
+    string playerInput = Console.ReadLine();
+    bool isValid = int.TryParse(playerInput, out int playerNumber);
+    if (isValid)
+    {
+        if (playerNumber == computernumber)
+        {
+            Console.WriteLine("You guessed it!");
+            break;
+        }
+        else if (playerNumber > computernumber)
+        {
+            Console.WriteLine("Too High");
+        }
+        else
+        {
+            Console.WriteLine("Too Low");
+        }
+    }
+}
